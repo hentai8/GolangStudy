c5f3e11eeb3c50143658aaa6d39bd4ad6348f579
hentai8 <496983136@qq.com>
2023-08-29 16:52:32 +0800

    feature: aleo miner0 miner1 test

--- aleo/miner.go
+++ aleo/miner.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "54.84.144.248:40000")
+	conn, err := net.Dial("tcp", "127.0.0.1:9096")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,7 +31,8 @@ func main() {
 		//_, err := conn.Write([]byte("hello\n"))
 		time.Sleep(1 * time.Second)
 
-		subscribe := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.subscribe\", \"params\": [\"gsa0/1.0\", \"AleoStratum/2.0.0\", null]}\n"
+		//subscribe := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.subscribe\", \"params\": [\"gsa0/1.0\", \"AleoStratum/2.0.0\", null]}\n"
+		subscribe := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.subscribe\", \"params\": [\"miner0\", \"AleoStratum/2.0.0\", null]}\n"
 
 		_, err := conn.Write([]byte(subscribe))
 		if err != nil {
@@ -47,7 +48,8 @@ func main() {
 
 		time.Sleep(1 * time.Second)
 
-		authorize := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.authorize\", \"params\": [\"huang93.hmc\", \"123\"]}\n"
+		authorize := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.authorize\", \"params\": [\"aleo1p4jjees0xj2m6hxlgej776elcm74lwlj4m3xuqk9f6zq64zjwgpslkwd42\", \"123\"]}\n"
+		//authorize := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.authorize\", \"params\": [\"miner0\", \"chainsAb@\"]}\n"
 
 		_, err = conn.Write([]byte(authorize))
 		if err != nil {
@@ -57,7 +59,8 @@ func main() {
 
 		time.Sleep(5 * time.Second)
 
-		submit := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.submit\", \"params\": [\"aleo1p4jjees0xj2m6hxlgej776elcm74lwlj4m3xuqk9f6zq64zjwgpslkwd42\", \"2298959d\", \"2298959d\", \"COMMITMENT\", \"PROOF\"]}\n"
+		submit := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.submit\", \"params\": [\"123\", \"123\", \"123\"]}\n"
+		//submit := "{\"jsonrpc\":\"2.0\",\"id\": 1, \"method\": \"mining.big_data\", \"params\": [\"123\", \"123\", \"big_data123\"]}\n"
 
 		_, err = conn.Write([]byte(submit))
 		if err != nil {
